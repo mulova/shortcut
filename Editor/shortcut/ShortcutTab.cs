699d37c6de8c5f12addd1b05776b322a48c19bc6
mulova <mulova@gmail.com>
2021-09-09 09:10:22 +0900

    2021.2 fix

--- Editor/shortcut/ShortcutTab.cs
+++ Editor/shortcut/ShortcutTab.cs
@@ -9,11 +9,8 @@ using UnityEngine;
 using UnityEngine.Ex;
 using Object = UnityEngine.Object;
 using System.Ex;
-#if UNITY_2021_2_OR_NEWER
 using UnityEditor.SceneManagement;
-#else
 using UnityEditor.Experimental.SceneManagement;
-#endif
 using System;
 
 namespace mulova.shortcut
